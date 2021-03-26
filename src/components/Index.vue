51643b4fd9621699b3b9590596586f98e1018a82
ahao <13370933172@163.com>
2021-03-26 11:26:21 +0800

    首页侧边栏

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,15 +1,123 @@
 <template>
-  <div class="index">
-    这是首页
-  </div>
+  <el-container class="index">
+    <el-header>
+      <div class="logo">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <div class="title">
+        <h1>后台管理系统</h1>
+      </div>
+      <div class="loginout">
+        欢迎~
+        <a @click="loginout" href="javascript:;">退出</a>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          unique-opened
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="2-1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="2-2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    loginout () {
+      this.$confirm('确定要退出登录吗?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          this.$message({
+            type: 'success',
+            message: '已退出!'
+          })
+          localStorage.removeItem('token')
+          this.$router.push('/login')
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消'
+          })
+        })
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.index {
+  height: 100%;
+  .el-header {
+    background-color: #b3c0d1;
 
+    display: flex;
+    .logo {
+      width: 180px;
+      img {
+        height: 40px;
+        margin: 10px;
+      }
+    }
+    .title {
+      flex: 1;
+      height: 60px;
+      line-height: 60px;
+      text-align: center;
+    }
+    .loginout {
+      width: 180px;
+      text-align: right;
+      font-weight: 700;
+      height: 60px;
+      line-height: 60px;
+      a {
+        color: orange;
+      }
+    }
+  }
+
+  .el-aside {
+    background-color: rgb(84, 92, 100);
+  }
+
+  .el-main {
+    background-color: #e9eef3;
+  }
+}
 </style>
